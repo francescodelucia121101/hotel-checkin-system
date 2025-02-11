e83ced1bc7cd03dfe421ad8e0a93887852c3d7e5
francescodelucia121101 <157105083+francescodelucia121101@users.noreply.github.com>
2025-02-11 10:31:08 +0100

    Add files via upload

---
+++ frontend/pages/index.js
@@ -0,0 +1,9 @@
+// pages/index.js
+export default function Home() {
+  return (
+    <div>
+      <h1>Benvenuto al sistema di check-in!</h1>
+      <p>Effettua il check-in e gestisci il tuo soggiorno.</p>
+    </div>
+  );
+}
