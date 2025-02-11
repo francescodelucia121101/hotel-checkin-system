44d32b2d0679d13d0ce6640ee0fb87d0cd5786e8
francescodelucia121101 <157105083+francescodelucia121101@users.noreply.github.com>
2025-02-11 12:59:01 +0100

    Update checkin.service.ts

--- backend/src/checkin/checkin.service.ts
+++ backend/src/checkin/checkin.service.ts
@@ -10,12 +10,16 @@ export class CheckinService {
     private readonly checkinRepository: Repository<Checkin>,
   ) {}
 
-  async createCheckin(data: Partial<Checkin>): Promise<Checkin> {
-    const newCheckin = this.checkinRepository.create(data);
-    return this.checkinRepository.save(newCheckin);
-  }
-
   async findAll(): Promise<Checkin[]> {
     return this.checkinRepository.find();
+  }
+
+  async findOne(id: number): Promise<Checkin> {
+    return this.checkinRepository.findOne(id); // Metodo per recuperare un singolo check-in
+  }
+
+  async create(checkinData: any): Promise<Checkin> {
+    const newCheckin = this.checkinRepository.create(checkinData);
+    return this.checkinRepository.save(newCheckin); // Metodo per creare un nuovo check-in
   }
 }
