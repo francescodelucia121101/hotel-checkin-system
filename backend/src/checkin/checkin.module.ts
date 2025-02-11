fe6779651f0f3c24fd3a73516183f69de6346402
francescodelucia121101 <157105083+francescodelucia121101@users.noreply.github.com>
2025-02-11 12:50:57 +0100

    Update checkin.module.ts

--- backend/src/checkin/checkin.module.ts
+++ backend/src/checkin/checkin.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CheckinService } from './checkin.service';
 import { CheckinController } from './checkin.controller';
+import { CheckinService } from './checkin.service';
 import { Checkin } from './checkin.entity';
+import { WubookModule } from '../wubook/wubook.module'; // <-- Importa il modulo Wubook
+
+
 
 @Module({
   imports: [TypeOrmModule.forFeature([Checkin])], // Aggiunto il repository Checkin
