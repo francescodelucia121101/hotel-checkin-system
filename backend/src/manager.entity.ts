bb0e14c9f6d614e87b1bd5de86b34f43b5274b44
francescodelucia121101 <157105083+francescodelucia121101@users.noreply.github.com>
2025-02-11 11:11:27 +0100

    Update manager.entity.ts

--- backend/src/manager.entity.ts
+++ backend/src/manager.entity.ts
@@ -1,26 +1,43 @@
-// src/manager.entity.ts
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity()
 export class Manager {
   @PrimaryGeneratedColumn()
-  id: number;
+  id!: number;
 
   @Column()
-  hotelName: string;
+  hotelName!: string;
 
   @Column()
-  managerEmail: string;
+  managerEmail!: string;
 
   @Column()
-  password: string;  // Assicurati di criptare la password in produzione
+  password!: string;  // Assicurati di criptare la password in produzione
 
   @Column()
-  wubookApiKey: string;
+  wubookApiKey!: string;
 
   @Column()
-  stripeApiKey: string;
+  stripeApiKey!: string;
 
   @Column()
-  hikvisionApiKey: string;
+  hikvisionApiKey!: string;
+
+  constructor(
+    id: number,
+    hotelName: string,
+    managerEmail: string,
+    password: string,
+    wubookApiKey: string,
+    stripeApiKey: string,
+    hikvisionApiKey: string,
+  ) {
+    this.id = id;
+    this.hotelName = hotelName;
+    this.managerEmail = managerEmail;
+    this.password = password;
+    this.wubookApiKey = wubookApiKey;
+    this.stripeApiKey = stripeApiKey;
+    this.hikvisionApiKey = hikvisionApiKey;
+  }
 }
