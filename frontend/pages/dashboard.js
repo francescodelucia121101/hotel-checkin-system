a617165536a369738826a83f042830d08d1aa380
francescodelucia121101 <157105083+francescodelucia121101@users.noreply.github.com>
2025-02-13 13:08:09 +0100

    Update dashboard.js

--- frontend/pages/dashboard.js
+++ frontend/pages/dashboard.js
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react";
 import { Container, Tabs, Tab, Box } from "@mui/material";
+import Sidebar from "../components/Sidebar";
 import StructureConfig from "../components/StructureConfig";
 import RoomsConfig from "../components/RoomsConfig";
 import DoorsConfig from "../components/DoorsConfig";
+import IntegrationsConfig from "../components/IntegrationsConfig";
 import axios from "axios";
 
 export default function Dashboard() {
@@ -27,20 +29,23 @@ export default function Dashboard() {
   };
 
   return (
-    <Container maxWidth="lg">
-      <h1 style={{ textAlign: "center", marginBottom: "20px" }}>Dashboard Manager</h1>
+    <Container maxWidth="xl" sx={{ display: "flex" }}>
+      <Sidebar setTab={setTab} />
+      <Box sx={{ flexGrow: 1, p: 4 }}>
+        <h1>Dashboard Manager</h1>
+        <Tabs value={tab} onChange={(e, newValue) => setTab(newValue)} centered>
+          <Tab label="Gestione Struttura" />
+          <Tab label="Gestione Camere" disabled={!selectedStructure} />
+          <Tab label="Gestione Porte" disabled={!selectedStructure} />
+          <Tab label="Integrazioni" />
+        </Tabs>
 
-      {/* Navigazione tra schede */}
-      <Tabs value={tab} onChange={(e, newValue) => setTab(newValue)} centered>
-        <Tab label="Gestione Struttura" />
-        <Tab label="Gestione Camere" disabled={!selectedStructure} />
-        <Tab label="Gestione Porte" disabled={!selectedStructure} />
-      </Tabs>
-
-      <Box sx={{ mt: 4 }}>
-        {tab === 0 && <StructureConfig onStructureSelect={setSelectedStructure} />}
-        {tab === 1 && selectedStructure && <RoomsConfig structure={selectedStructure} />}
-        {tab === 2 && selectedStructure && <DoorsConfig structure={selectedStructure} />}
+        <Box sx={{ mt: 4 }}>
+          {tab === 0 && <StructureConfig onStructureSelect={setSelectedStructure} />}
+          {tab === 1 && selectedStructure && <RoomsConfig structure={selectedStructure} />}
+          {tab === 2 && selectedStructure && <DoorsConfig structure={selectedStructure} />}
+          {tab === 3 && <IntegrationsConfig />}
+        </Box>
       </Box>
     </Container>
   );
