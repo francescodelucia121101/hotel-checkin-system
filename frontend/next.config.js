28813ca71776f560fecfb4803f19ab8bd8b312f1
francescodelucia121101 <157105083+francescodelucia121101@users.noreply.github.com>
2025-02-13 07:23:57 +0100

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,17 @@
-module.exports = {
-  webpack(config, { isServer }) {
-    if (!isServer) {
-      // Escludi cross-spawn dal bundle client-side
-      config.resolve.alias['cross-spawn'] = false;
-    }
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      child_process: false,
+      readline: false,
+      buffer: require.resolve("buffer/"),
+      stream: require.resolve("stream-browserify"),
+      util: require.resolve("util/"),
+    };
     return config;
   },
 };
+
+module.exports = nextConfig;
