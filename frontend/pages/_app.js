a9864a1280a2f1b9bb2c3141706eb4fbe8163e3f
francescodelucia121101 <157105083+francescodelucia121101@users.noreply.github.com>
2025-02-14 09:26:36 +0100

    Update _app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./dashboard";
 import BookingDetails from "./booking-details";
 import dynamic from "next/dynamic";
 
